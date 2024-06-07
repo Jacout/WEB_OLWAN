f9c64825bd40f5248308d841a58610a80ada97b3
Jacob Rodriguez <misaelt_99@outlook.com>
2024-06-06 22:56:04 -0600

    primera version pagina

---
+++ script.js
@@ -0,0 +1,4 @@
+// Agrega un evento de clic al botón de "Descubre más"
+document.querySelector('button').addEventListener('click', function() {
+    alert('¡Hola! Estamos aquí para ayudarte.');
+});
